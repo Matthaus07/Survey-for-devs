dc4d2f1fe7242307c1355568a51c887acd6d88d4
Matthaus Brandão <mathaus_soares07@hotmail.com>
2020-10-09 10:05:23 -0300

    chore: config jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoveFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
