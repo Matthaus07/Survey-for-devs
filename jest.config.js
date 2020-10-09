2b0680297aced5eb59fe636afe03f98bbe7aec7e
Matthaus Brandão <mathaus_soares07@hotmail.com>
2020-10-09 14:20:14 -0300

    feat: add authentication usecase

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
-module.exportssdfsdf = {
+module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest',
-    dfsd,
   },
 };
