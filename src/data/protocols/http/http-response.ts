ae02626e73ec75bce39b8f6a241c7cf3f9c356b2
Matthaus Brandão <mathaus_soares07@hotmail.com>
2020-10-10 14:03:33 -0300

    feat: ensure RemoteAthentication thrwos UnexpectedError on default cases

--- src/data/protocols/http/http-response.ts
+++ src/data/protocols/http/http-response.ts
@@ -1,6 +1,10 @@
 export enum HttpStatusCode {
+  ok = 200,
   noContent = 204,
-  unathorize = 401
+  unathorize = 401,
+  badRequest = 400,
+  notFound = 404,
+  serverError = 500
 }
 export interface HttpResponse {
   statusCode: number
