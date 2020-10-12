3680c1066da648220a05004ba41a1ecabfab052c
Matthaus Brandão <mathaus_soares07@hotmail.com>
2020-10-12 19:10:35 -0300

    refactor: ajust form-status-styles name

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import Styles from '../login/login-styles.scss'
-import Header from '@/presentation/components/login-header/login-header'
-import Footer from '@/presentation/components/footer/footer'
-import Input from '@/presentation/components/input/input'
-import FormStatus from '@/presentation/components/form-status/form-status'
+import { Input, LoginHeader, Footer, FormStatus } from '@/presentation/components'
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
-      <Header/>
+      <LoginHeader/>
       <form className={Styles.form}>
         <h2>Login</h2>
-        <Input type="email" name="email" placeholder="Digite seu Email"/>
 
+        <Input type="email" name="email" placeholder="Digite seu Email"/>
         <Input type="password" name="password" placeholder="Digite sua senha"/>
 
         <button className={Styles.submit} type="submit">Entrar</button>
         <span className={Styles.link}>Criar Conta</span>
+
         <FormStatus />
       </form>
       <Footer/>
