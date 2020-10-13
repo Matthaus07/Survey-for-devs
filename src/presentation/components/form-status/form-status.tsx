6eea083aaf556f9fd24ef982ee8841eb63eb27ac
Matthaus Brandão <mathaus_soares07@hotmail.com>
2020-10-13 01:08:01 -0300

    refactor: ajust form status for login

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,13 +1,17 @@
-import React, { memo } from 'react'
+
+import React, { memo, useContext } from 'react'
 import Spinner from '../spinner/spinner'
 import Styles from './form-status-styles.scss'
-
+import Context from '@/presentation/context/form/context-form'
 const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(Context)
   return (
-    <div className={Styles.errorWrap}>
-      <Spinner className={Styles.spinner} />
-      <span className={Styles.error}>Error</span>
-    </div>)
+
+    <div data-testid="error-wrap" className={Styles.errorWrap}>
+
+      { isLoading && <Spinner className={Styles.spinner} />}
+      { errorMessage && <span className={Styles.error}>Error</span>
+      }    </div>)
 }
 
 export default memo(FormStatus)
