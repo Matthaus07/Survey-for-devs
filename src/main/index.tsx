0231bc816985a90553ce9895ea6bce66ac58fa10
Matthaus Brandão <mathaus_soares07@hotmail.com>
2020-10-13 01:06:31 -0300

    feat: ensure login starts with submit button disabled

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,7 @@
 import Login from '@/presentation/pages/login/login'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import '@/presentation/styles/global.scss'
 
 ReactDOM.render(
   <Login/>,
