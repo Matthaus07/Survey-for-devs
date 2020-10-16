aeeae9961a293e37892e3ab79b76ea746acd18f2
Matthaus Brandão <mathaus_soares07@hotmail.com>
2020-10-16 20:07:07 -0300

    test: ensure MinLengthValidation returns falsy if email is invalid

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,16 @@
+import { InvalidFieldError } from '@/validation/error'
+import { MinLengthValidation } from './min-length-validation'
+
+describe('MinLengthValidation', () => {
+  test('should return error if value is invalid', () => {
+    const sut = new MinLengthValidation('field', 5)
+    const error = sut.validate('123')
+    expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('should return falsy if value is invalid', () => {
+    const sut = new MinLengthValidation('field', 5)
+    const error = sut.validate('12345')
+    expect(error).toBeFalsy()
+  })
+})
