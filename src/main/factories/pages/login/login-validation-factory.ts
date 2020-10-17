6b6e330246c97c9ff9bec3accab6179ea1ba74d5
Matthaus Brandão <mathaus_soares07@hotmail.com>
2020-10-17 17:21:10 -0300

    refactor: change name ValidationBuilder for Builder in makeLoginValidation

--- src/main/factories/pages/login/login-validation-factory.ts
+++ src/main/factories/pages/login/login-validation-factory.ts
@@ -3,7 +3,7 @@ import { ValidationBuilder as Builder } from '@/validation/validators/builder/va
 
 export const makeLoginValidation = (): ValidationComposite => {
   return ValidationComposite.build([
-    ...Builder.field('email').required().build(),
+    ...Builder.field('email').required().email().build(),
     ...Builder.field('password').required().min(5).build()
   ])
 }
