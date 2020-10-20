6f73049b268fa51cb00c493d98a04ab502bd153c
Matthaus Brandão <mathaus_soares07@hotmail.com>
2020-10-20 10:33:58 -0300

    test: ensure RemoteAddAccount throws unexpectedError if httPostClient returns 400 and 500

--- src/data/usecases/add-account/add-account.spec.ts
+++ src/data/usecases/add-account/add-account.spec.ts
@@ -4,6 +4,9 @@ import { AddAccountParams } from '@/domain/usecases'
 import { RemoteAddAccount } from './add-account'
 import faker from 'faker'
 import { mockAddAccount } from '@/domain/test/mock-add-account'
+import { HttpStatusCode } from '@/data/protocols/http'
+import { EmailAlreadyInUse } from '@/domain/errors/email-in-use-error'
+import { UnexpectedError } from '@/domain/errors'
 interface SutTypes{
   sut: RemoteAddAccount
   httpPostClientSpy: HttpPostClientSpy<AddAccountParams, AccountModel>
@@ -28,5 +31,31 @@ describe('AddAccount', () => {
     const { sut, httpPostClientSpy } = makeSut()
     await sut.add(mockAccount)
     expect(httpPostClientSpy.body).toEqual(mockAccount)
+  })
+  test('should throw Email error if HttpPostClient returns 403', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden
+    }
+    const promise = sut.add(mockAddAccount())
+    await expect(promise).rejects.toThrow(new EmailAlreadyInUse())
+  })
+
+  test('should throw UnexpectedError if HttpPostClient returns 400', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest
+    }
+    const promise = sut.add(mockAddAccount())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  test('should throw UnexpectedError if HttpPostClient returns 500', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest
+    }
+    const promise = sut.add(mockAddAccount())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
