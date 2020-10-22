72e72b422b76cf5ecffe9b7123d37e7b478eda6f
Matthaus Brandão <mathaus_soares07@hotmail.com>
2020-10-22 12:11:57 -0300

    feat: ensure SignUp shows spinner on submit

--- src/presentation/test/form-helper.ts
+++ src/presentation/test/form-helper.ts
@@ -1,14 +1,6 @@
 
-import { fireEvent, RenderResult, waitFor } from '@testing-library/react'
+import { fireEvent, RenderResult } from '@testing-library/react'
 import faker from 'faker'
-
-export const simulateValidSubmit = async (sut: RenderResult, email = faker.internet.email(), password = faker.internet.password()): Promise<void> => {
-  populateField(sut,'email', email)
-  populateField(sut,'password', password)
-  const form = sut.getByTestId('form')
-  fireEvent.submit(form)
-  await waitFor(() => form)
-}
 
 export const populateField = (sut: RenderResult,fieldName: string, value = faker.random.word()): void => {
   const emailInput = sut.getByTestId(fieldName)
