333a2f61ed8a63b5d4fc33ed1c524e27cf703d73
Matthaus Brandão <mathaus_soares07@hotmail.com>
2020-10-27 15:37:13 -0300

    test: ensure Login present error message if valid credentials are provided

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,5 @@
 import faker from 'faker'
-
+const baseUrl: string = Cypress.config().baseUrl
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
@@ -28,5 +28,16 @@ describe('Login', () => {
     cy.getByTestId('password-status').should('have.attr','title','ok')
     cy.getByTestId('submit').should('not.have.attr','disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+  it('Should present error if valid credentials are provided', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').click()
+    cy.getByTestId('error-wrap')
+    cy.getByTestId('spinner').should('exist')
+    cy.getByTestId('main-error').should('not.exist')
+    cy.getByTestId('spinner').should('not.exist')
+    cy.getByTestId('main-error').should('contain.text','credenciais inválidas')
+    cy.url().should('eq',`${baseUrl}/login`)
   })
 })
